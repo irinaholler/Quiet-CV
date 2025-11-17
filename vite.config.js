de2bcb09a143055ac7e8f22d3a96a8964220830f
Irina Holler <irinaniko@hotmail.com>
2025-11-17 10:13:51 +0100

    Fixed some bugs

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-})
+  // Dev: http://localhost:5173/
+  // Build: works under https://myrina.de/quietcv/
+  base: mode === "production" ? "/quietcv/" : "/",
+}));
